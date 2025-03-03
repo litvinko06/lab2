9d4bf9ab2e69acacc360cab508871b383aee21bf
COD-ETheProgram <justkalexsay@gmail.com>
2025-03-04 01:30:26 +0200

    Add files via upload

---
+++ lab2.1/Program2.cs
@@ -0,0 +1,22 @@
+﻿class Person
+{
+    protected string phoneNumber = "+380-XX-XXX-XX-XX";
+    protected string personName = "Alex K. O.";
+
+    public virtual void GetInfo()
+    {
+        Console.WriteLine($"Name: {personName}");
+        Console.WriteLine($"Number: {phoneNumber}");
+    }
+}
+
+class Deanery : Person
+{
+    public string id = "M2NSB5I24GSG";
+
+    public override void GetInfo()
+    {
+        base.GetInfo();
+        Console.WriteLine($"Student ID: {id}");
+    }
+}
