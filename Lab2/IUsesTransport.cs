9bfe07c8a6860f210012134ba7e17d7e57769326
litvinko06 <litvinko.karyna@lll.kpi.ua>
2025-03-03 19:46:33 +0200

    Додавання існуючого проєкту

---
+++ Lab2/IUsesTransport.cs
@@ -0,0 +1,4 @@
+public interface IUsesTransport
+{
+    void UseTransport();
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Transport bus = new Bus("Mercedes Sprinter", 20);
+        Student student = new Student("Андрій", bus);
+        
+        student.UseTransport();
+    }
+}
