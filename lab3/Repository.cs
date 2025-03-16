a0dfd8ed3ac071a199e54896038ef915bb93477d
COD-ETheProgram <justkalexsay@gmail.com>
2025-03-16 16:37:01 +0200

    Update Repository.cs

--- lab3/Repository.cs
+++ lab3/Repository.cs
@@ -1,6 +1,20 @@
 ﻿// Загальний клас, що реалізує CRUD
-public class Repository<T>
+public class Repository<T> :IRepository<T>
 {
+  public void Commit(T value1)
+  {
+    Console.WriteLine($"Initial commit - {value1}");
+  }
 
+  public void Commit(T value1, T value2)
+  {
+    if (value1.Equals(value2) != true)
+    {
+        Console.WriteLine($"Commited changes: {value1} => {value2}");
+    }
+    else
+    {
+        Console.WriteLine("No commit changes");
+    }
+  }
 }
-
