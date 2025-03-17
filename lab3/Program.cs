f130fe21814425b4e198679470d06956b2948b4c
COD-ETheProgram <justkalexsay@gmail.com>
2025-03-18 00:22:32 +0200

    Update Program.cs

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -10,17 +10,6 @@ class Program_Main
         Repository<int> myCode = new Repository<int>();
         var commits = myCode.GetCommit();
 
-        /*myCode.Commit("test");
-        foreach (var commit in commits)
-        {
-            Console.WriteLine(commit);
-        }
-        myCode.Commit("test", "test: the sequel");
-        foreach (var commit in commits)
-        {
-            Console.WriteLine(commit);
-        }*/
-
         int number = 0;
         for (int i = 0; i < 10; i++)
         {
@@ -61,51 +50,3 @@ class Program_Main
         }
     }
 }
-
-// Це для тестування та розуміння як працює, потім приберемо
-/*public class Calculator<T>
-{
-    public static bool AreEqual(T value1, T value2)
-    {
-        return value1.Equals(value2);
-    }
-}
-
-class Geeks
-{
-
-    public static void Main(String[] args)
-    {
-        // Creating a List of integers
-        List<int> l = new List<int>();
-
-        // Adding elements to the List one by one
-        l.Add(1);
-        l.Add(2);
-        l.Add(3);
-        l.Add(4);
-        l.Add(5);
-        l.Add(6);
-        l.Add(7);
-        l.Add(8);
-        l.Add(9);
-
-        // Printing the Capacity and Count of l
-        Console.WriteLine("Capacity: " + l.Capacity);
-        Console.WriteLine("Count: " + l.Count);
-
-        bool intEqual = Calculator<int>.AreEqual(4, 4);
-        bool strEqual = Calculator<string>.AreEqual("Interview", "Happy");
-
-        Console.WriteLine(intEqual);
-        Console.WriteLine(strEqual);
-
-        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
-        List<int> evenNumbers = numbers.FindAll(x => x % 2 == 0);
-
-        foreach (var item in evenNumbers)
-        {
-            Console.WriteLine(item);
-        }
-    }
-}*/
