5fb8e4789720d96bc70721194d2ad3b69a3b40be
COD-ETheProgram <justkalexsay@gmail.com>
2025-03-18 00:21:44 +0200

    Add files via upload

---
+++ lab3/Repository.cs
@@ -0,0 +1,32 @@
+﻿// Загальний клас, що реалізує CRUD
+public class Repository<T> :IRepository<T>
+{
+    List<T> commitList = [];
+
+    public void Commit(T value1)
+    {
+        commitList.Add(value1);
+
+        Console.WriteLine($"Initial commit - {value1}");
+    }
+
+    public void Commit(T value1, T value2)
+    {
+        if (value1.Equals(value2) != true)
+        {
+            commitList.Remove(value1);
+            commitList.Add(value2);
+
+            Console.WriteLine($"Commit changes: {value1} => {value2}");
+        }
+        else
+        {
+            Console.WriteLine("No commit changes");
+        }
+    }
+
+    public List<T> GetCommit()
+    {
+        return commitList;
+    }
+}
