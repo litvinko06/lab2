faa6ae09780538025db31ab88a7ec4861b328fe0
litvinko06 <litvinko.karyna@lll.kpi.ua>
2025-03-24 15:20:10 +0200

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,11 @@
+namespace lab4;
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+   
+
