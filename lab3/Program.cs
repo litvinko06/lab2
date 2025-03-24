d44c50a55386d334a3bb27bddb1c2bf80b810bbf
COD-ETheProgram <justkalexsay@gmail.com>
2025-03-24 12:11:14 +0200

    Update Program.cs

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -6,47 +6,38 @@ class Program_Main
 {
     static void Main()
     {
-        Random rnd = new Random();
         Repository<int> myCode = new Repository<int>();
         var commits = myCode.GetCommit();
-
+        Random rnd = new Random();
         int number = 0;
+
         for (int i = 0; i < 10; i++)
         {
             number = rnd.Next(1, 21);
-            myCode.Commit(number);
+            myCode.Add(number);
         }
-        Console.WriteLine("");
-        foreach (int commit in commits)
+
+        Console.WriteLine("List:");
+        foreach (var commit in commits)
         {
             Console.WriteLine(commit);
         }
         Console.WriteLine("");
 
-        myCode.Commit(number, rnd.Next(1, 21));
-        Console.WriteLine("");
-        foreach (int commit in commits)
+        myCode.Delete(number);
+        Console.WriteLine("List without last element:");
+        foreach (var commit in commits)
         {
             Console.WriteLine(commit);
         }
         Console.WriteLine("");
 
         List<int> evenCommits = commits.FindAll(x => x % 2 == 0);
+        Console.WriteLine("Even numbers in List:");
         foreach (int evencommit in evenCommits)
         {
             Console.WriteLine(evencommit);
         }
         Console.WriteLine("");
-
-        int n = 5;
-        int j = 0;
-        foreach (int commit in commits)
-        {
-            if (j == n)
-            {
-                Console.WriteLine(commit);
-            }
-            j++;
-        }
     }
 }
